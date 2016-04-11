0b5895e059cdc24fdddb1ee116da46bb7c8eed21
Riccardo Zanol <ricc@zanol.eu>
2016-04-11 15:40:39 +0200

    Fix the power normalization

--- matlab/filter_from_white.m
+++ matlab/filter_from_white.m
@@ -1,4 +1,4 @@
-function y = filter_from_white(b, a, N, M, seed)
+function y = filter_from_white(b, a, N, M, Tp, seed)
 rngstate = rng(seed);
 w = sqrt(0.5) .* (randn(N, M) + 1j*randn(N, M));
 rng(rngstate);
@@ -6,6 +6,7 @@ rng(rngstate);
 % Random proc with the PSD specified by H = B/A
 y_ = filter(b, a, w, [], 2);
 H = freqz(b, a, 'whole', M);
-H_energy = sum(abs(H).^2);
-y = y_ ./ sqrt(H_energy); % Normalize the PSD to integrate to 1
+H_energy = Tp * sum(abs(H).^2);
+H_power = H_energy / (Tp*M);
+y = y_ ./ sqrt(H_power); % Normalize the PSD to integrate to 1
 end
